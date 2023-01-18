b8a37d957ea4bda4418f66ec5bc53a5821fdfa61
Siho Choi <78015683+SihoChoii@users.noreply.github.com>
2023-01-18 21:05:31 +0000

    v1.1

--- Hotel.java
+++ Hotel.java
@@ -9,13 +9,56 @@ public class Hotel
     public Hotel (String hotelName, int totalRooms, int floors)
     {
         this.rooms = new Room[totalRooms];
+        for (int i = 0; i < floors; i++)
+        {
+            for (int p = 0; p < totalRooms / floors; p++)
+            {
+                String tempRoomType;
+                if (p == (totalRooms / floors)-1)
+                {
+                    tempRoomType = "suite";
+                }
+                else if (p >= (totalRooms / floors) - 5)
+                {
+                    tempRoomType = "single king";
+                }
+                else
+                {
+                    tempRoomType = "double qeen";
+                }
+                this.rooms[i*(totalRooms / floors)+p] = new Room(i*100 + p, tempRoomType);
+            }
+        }
+    }
+
+    public int getTotalRooms()
+    {
+        return totalRooms;
+    }
+    public int getNumberOccupied()
+    {
+        int occupiedNumber = 0;
         for (int i = 0; i < totalRooms; i++)
         {
-            if (i = totalRooms/floors)
-            (
-                 
-            )
-            this.rooms[i] = new Room(totalRooms, null);
+            if (this.rooms[i].getOccupantName == null)
+            {
+                occupiedNumber++;
+            }
+        }
+        return occupiedNumber;
+    }
+    public double getOccupantRate()
+    {
+        return ((double)this.getNumberOccupied() / (double)this.totalRooms);
+    }
+    public boolean rentRoom(String roomType, String renterName, int daysStay)
+    {
+        for (int i = 0; i < this.totalRooms; i++)
+        {
+            if (this.rooms[i].getOccupantName() == null && this.rooms[i].getRoomType == roomType)
+            {
+                this.rooms[i].setOccupant(renterName, daysStay);
+            }
         }
     }
 }
